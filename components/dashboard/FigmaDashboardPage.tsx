772aff5a972333483dbbf6cd9fa5e2e242649764
root <root@server1.cartandget.com>
2026-02-19 18:49:12 +0000

    fix

--- components/dashboard/FigmaDashboardPage.tsx
+++ components/dashboard/FigmaDashboardPage.tsx
@@ -187,6 +187,7 @@ const FigmaDashboardPage: React.FC<FigmaDashboardPageProps> = ({
 
         {/* Analytics Bar Chart */}
         <div className="px-2 xs:px-3 sm:px-4 lg:px-3 xl:px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-12 gap-2 xs:gap-3 sm:gap-4">
           <VisitorStatsSection
             // timeFilter={timeFilter}
             // onTimeFilterChange={(filter) => setTimeFilter(filter as any)}
@@ -201,7 +202,7 @@ const FigmaDashboardPage: React.FC<FigmaDashboardPageProps> = ({
           //   onDateRangeChange={(range) => setSelectedMonth(range.start)}
           //   tenantId={tenantId}
           />
-       
+       </div>
         </div>
 
          
